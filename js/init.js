91f86b6ed9bd35d3d138dd748b6603efcc3e69a6
bucabay <bucabay@e6c322b0-92a2-11dd-aac2-07b17bfeb51a>
2009-06-06 15:35:17 +0000

    + added fake cursor and input 	input is proxied from a hidden inputbox instead of reading keypresses 	using String.fromCharCode(event.keyCode) only works for unicode so implementing it would be complicated 	I also cant find a way to detect if caps lock is on * fixed a bug in xhr.js calling this.debug() in the wrong scope * added the prefix phpshell- to HTML elements to prevent conflicts if embedded

--- js/init.js
+++ js/init.js
@@ -8,21 +8,106 @@ phpshell.client = {
 	// initiate
 	init: function() {
 		window.onload = function() {
-			var input = document.getElementById('cmd');
-			input.onkeyup = function(e) {
-				e = (!e) ? window.event : e;
-	    		target = (!e.target) ? e.srcElement : e.target;
+			
+			// get dom references
+			phpshell.client.cursor = document.getElementById('phpshell-cursor');
+			phpshell.client.input = document.getElementById('phpshell-input');
+			phpshell.client.hiddeninput = document.getElementById('phpshell-hinput');
+			phpshell.client.shell = document.getElementById('phpshell-shell');
+			phpshell.client.results = document.getElementById('phpshell-results');
+			
+			// activate shell if user clicks on it
+			phpshell.client.shell.onclick = function(e) {
+				e = e || window.event;
+				phpshell.client.active = true;
+				phpshell.client.setCursorBlink(true);
+				phpshell.client.hiddeninput.focus();
+				e.cancelBubble = true;
+				if (e.stopPropagation) e.stopPropagation();
+			};
+			// if user clicks anywhere else, deactivate shell
+			document.onclick = function() {
+				phpshell.client.active = false;
+				phpshell.client.setCursorBlink(false);
+			};
+			// catch all keypresses
+			document.onkeyup = function(e) {
+				if (!phpshell.client.active) {
+					return;
+				}
+				e = e || window.event;
+	    		target = e.srcElement || e.target;
 				// enter pressed, without shift
 				if (!e.shiftKey && e.keyCode == 13) {
+					// get current input
+					var input = phpshell.client.getInput();
 					// add command to screen
 					var prompt = phpshell.client.exec_as_php() ? 'php$ ' : '$ ';
-					phpshell.client.addEntry(prompt+this.value);
+					phpshell.client.addEntry(prompt+input);
 					// execute command
-					phpshell.client.sendCommand(this.value);
-					input.value = '';
+					phpshell.client.sendCommand(input);
+					phpshell.client.setInput('');
+					phpshell.client.clearHiddenInput();
+				} else {
+					phpshell.client.addInput(phpshell.client.getHiddenInput());
+					phpshell.client.clearHiddenInput();
+				}
+				if (e.keyCode == 8) {
+					phpshell.client.removeLastCharInput();
+					phpshell.client.removeLastCharHiddenInput();
 				}
 			};
 		};
+	},
+	
+	setInput: function(str) {
+		phpshell.client.input.innerHTML = str;
+	},
+	
+	getInput: function() {
+		return phpshell.client.input.innerHTML;
+	},
+	
+	addInput: function(str) {
+		phpshell.client.input.innerHTML += str;
+	},
+	
+	clearInput: function(str) {
+		phpshell.client.input.innerHTML = '';
+	},
+	removeLastCharInput: function() {
+		var input = phpshell.client.input;
+		input.innerHTML = input.innerHTML ? input.innerHTML.replace(/.$/, '') : '';
+	},
+	
+	getHiddenInput: function() {
+		return phpshell.client.hiddeninput.value;
+	},
+	
+	clearHiddenInput: function() {
+		phpshell.client.hiddeninput.value = '';
+	},
+	
+	removeLastCharHiddenInput: function() {
+		var input = phpshell.client.hiddeninput;
+		input.value = input.value ? input.value.replace(/.$/, '') : '';
+	},
+	
+	setCursor: function(str) {
+		phpshell.client.cursor.innerHTML = str;
+	},
+	
+	setCursorBlink: function(blink) {
+		clearInterval(phpshell.client.cursor_blink);
+		var cursor = phpshell.client.cursor;
+		if (blink) {
+			cursor.style.visibility = 'visible';
+			phpshell.client.cursor_blink = setInterval(function() {
+				cursor.style.visibility = cursor.style.visibility == 'hidden' ? 'visible' : 'hidden';
+			}, 500);
+		} else {
+			cursor.style.visibility = 'hidden';
+		}
 	},
 	
 	// check if we want to execute code as php
@@ -56,7 +141,7 @@ phpshell.client = {
 	
 	// add an etry
 	addEntry: function(str) {
-		var results = document.getElementById('results');
+		var results = phpshell.client.results;
 		var result = document.createElement('div');
 		results.appendChild(document.createTextNode(str));
 		results.appendChild(result);
